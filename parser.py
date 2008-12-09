5911ec13d0a7bd96a851b4f64301498640961dd6
Will Larson <lethain@gmail.com>
2008-12-08 23:05:49 -0500

    Further refactoring.

--- parser.py
+++ parser.py
@@ -14,6 +14,7 @@ from operator import itemgetter
 "NewsGatorOnline/2.0 (http://www.newsgator.com; 39 subscribers)"
 "Netvibes (http://www.netvibes.com/; 5 subscribers; feedId: 5404723)"
 
+127.0.0.1 - - [02/Dec/2008:09:11:06 -0600] "GET /feeds/flow/code/ HTTP/1.0" 200 66736 "-" "CFNetwork/129.22"
 """
 
 def restrict(lst, cutoff, count):
@@ -27,8 +28,12 @@ def restrict(lst, cutoff, count):
 def parse(filename):
     'Return tuple of dictionaries containing file data.'
     def make_entry(x):
-        return { 'uri':x.group(1) }
-    log_re = '"GET (.*?) HTTP/1.\d"'
+        return { 
+            'uri':x.group('uri'),
+            'time':x.group('time'),
+            }
+    # log_re = '"GET (?P<uri>.*?) HTTP/1.\d"'   HTTP/1.0" 200 66736 "-" "CFNetwork/129.22"'
+    log_re = '(?P<ip>[.\d]+) - - \[(?P<time>.*?)\] "GET (?P<uri>.*?)"'
     search = re.compile(log_re).search
     matches = (search(line) for line in file(filename))
     return (make_entry(x) for x in matches if x)
@@ -47,15 +52,16 @@ def print_results(lst):
     for item in lst:
         print "%s => %s" % (item[0], item[1])
 
-def hits(filename, cutoff, quantity):
+
+def generic_report_for_key(key, filename, cutoff, quantity):
     entries = parse(filename)
-    lst = count_value(entries, 'uri')
+    lst = count_value(entries, key)
     lst = sorted(lst, key=itemgetter(1), reverse=True)
     lst =  restrict(lst, cutoff, quantity)
     print_results(lst)
 
 def main():
-    p = OptionParser("usage: parser.py file hits|subscribers")
+    p = OptionParser("usage: parser.py file uri|time|subscribers")
     p.add_option('-c','--cutoff',dest='cutoff',
                  help="CUTOFF for minimum hits",
                  metavar="CUTOFF")
@@ -74,10 +80,10 @@ def main():
     cutoff = int(options.cutoff) if options.cutoff else None
     qty = int(options.quantity) if options.quantity else None
     
-    if report_type == 'hits':
-        hits(filename, cutoff=cutoff, quantity=qty)
-    elif report_type == 'subscribers':
+    if report_type == 'subscribers':
         subscribers(filename, cutoff=cutoff, quantity=qty)
+    elif report_type in ['uri','time']:
+        generic_report_for_key(report_type, filename, cutoff, qty)
     else:
         p.error("specified an invalid report type")
 
